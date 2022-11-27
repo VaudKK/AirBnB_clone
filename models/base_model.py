57ba1e4d59ad2c3301c99d8a1bb5c73def61190c
Vaud Keith <Vaudkeith@gmail.com>
2022-11-27 01:33:24 -0800

    feat: BaseModel complete

--- models/base_model.py
+++ models/base_model.py
@@ -8,16 +8,22 @@ class BaseModel:
     def __init__(self):
         """ Initialize """
         self.id = uuid.uuid4()
-        self.created_at = datetime.datetime
-        self.updated_at = datetime.datetime
+        self.created_at = datetime.datetime.now()
+        self.updated_at = datetime.datetime.now()
 
     def __str__(self):
         """ to string """
-        pass
+        return "[{}] ({}) {}".format(self.__class__.__name__, str(self.id), self.__dict__)
 
     def save(self):
         """ updates updated_at value """
-        self.updated_at = datetime.datetime
-    
-    def to_dict(self):
+        self.updated_at = datetime.datetime.now()
 
+    def to_dict(self):
+        """ to dictionary """
+        rdict = self.__dict__.copy()
+        rdict['created_at'] = self.created_at.isoformat()
+        rdict['updated_at'] = self.updated_at.isoformat()
+        rdict['__class__'] = self.__class__.__name__
+        
+        return rdict
